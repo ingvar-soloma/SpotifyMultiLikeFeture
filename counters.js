7ce682c340dc231baff365230e80028d7cd910c5
ingvar-soloma <ingvar.soloma@gmail.com>
2023-05-13 19:17:10 +0300

    added auto updating for counters

--- counters.js
+++ counters.js
@@ -2,6 +2,14 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     if (request.action === "getElements") {
         const likeButtons = document.querySelectorAll('[data-testid="add-button"][aria-checked="true"][tabindex="-1"]');
         const dislikeButtons = document.querySelectorAll('[data-testid="add-button"][aria-checked="false"][tabindex="-1"]');
-        sendResponse({ likeButtons: Array.from(likeButtons).length, dislikeButtons: Array.from(dislikeButtons).length });
+        const songsWithoutLikeList = Array.from(dislikeButtons).map(button => {
+            const songElement = button.closest('[data-testid="tracklist-row"]');
+            const songId = songElement.querySelector('[data-encore-id="type"]').textContent;
+            const songNameElement = songElement.querySelector('.standalone-ellipsis-one-line');
+            const songName = songNameElement ? songNameElement.textContent : '';
+            return { id: songId, name: songName };
+        });
+        sendResponse({ likeButtons: likeButtons.length, dislikeButtons: dislikeButtons.length, songsWithoutLikeList });
     }
 });
+
--- popup.js
+++ popup.js
@@ -1,16 +1,26 @@
 document.addEventListener("DOMContentLoaded", function () {
     const likeButton = document.getElementById("likeButton");
     const message = document.getElementById("message");
-    const counterContainer = document.getElementsByClassName("counter-container")[0];
+    const spotifySection = document.getElementById("spotify-section");
+    const songsListContainer = document.getElementById("songsListContainer");
 
     const dislikeCounter = document.getElementById("dislikeCounter");
     const likeCounter = document.getElementById("likeCounter");
+
+    // const sectionHeading = document.querySelector(".section-heading");
+    // const songSection = document.querySelector(".song-section");
+
+
+    // sectionHeading.addEventListener("click", function () {
+    //     songSection.classList.toggle("hidden");
+    // });
 
     likeButton.addEventListener("click", function () {
         chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
             chrome.tabs.executeScript(tabs[0].id, { file: "content.js" });
         });
     });
+
 
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
         const tab = tabs[0];
@@ -19,13 +29,15 @@ document.addEventListener("DOMContentLoaded", function () {
         if (tab && tab.url.includes("spotify.com")) {
             body.classList.remove("disabled");
             message.style.display = "none";
-            counterContainer.style.display = "flex";
+            spotifySection.style.display = "block";
 
             chrome.tabs.executeScript(tab.id, { file: "counters.js" }, function () {
                 chrome.tabs.sendMessage(tab.id, { action: "getElements" }, function (response) {
-                    const { likeButtons, dislikeButtons } = response;
-                    likeCounter.innerText = likeButtons.length;
-                    dislikeCounter.innerText = dislikeButtons.length;
+                    const { likeButtons, dislikeButtons, songsWithoutLikeList } = response;
+                    console.log(likeButtons, dislikeButtons, songsWithoutLikeList);
+                    likeCounter.innerText = likeButtons;
+                    dislikeCounter.innerText = dislikeButtons;
+                    fillSongsWithoutLikeList(songsWithoutLikeList);
                 });
             });
 
@@ -33,7 +45,43 @@ document.addEventListener("DOMContentLoaded", function () {
             body.classList.add("disabled");
             likeButton.disabled = true;
             message.style.display = "block";
-            counterContainer.style.display = "none";
+            spotifySection.style.display = "none";
         }
     });
+
+    function fillSongsWithoutLikeList(songsWithoutLikeList) {
+        songsListContainer.innerHTML = ""; // Clear the existing content
+
+        // Iterate over the songsWithoutLikeList and create list items
+        songsWithoutLikeList.forEach(song => {
+            const listItem = document.createElement("li");
+            listItem.textContent = `${song.id}: ${song.name}`;
+            songsListContainer.appendChild(listItem);
+        });
+    }
+
+    function updateUI() {
+        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+            const tab = tabs[0];
+
+            if (tab && tab.url.includes("spotify.com")) {
+                chrome.tabs.executeScript(tab.id, { file: "counters.js" }, function () {
+                    chrome.tabs.sendMessage(tab.id, { action: "getElements" }, function (response) {
+                        const { likeButtons, dislikeButtons, songsWithoutLikeList } = response;
+                        console.log(likeButtons, dislikeButtons, songsWithoutLikeList);
+                        likeCounter.innerText = likeButtons;
+                        dislikeCounter.innerText = dislikeButtons;
+                        fillSongsWithoutLikeList(songsWithoutLikeList);
+                    });
+                });
+            }
+        });
+    }
+
+    // Initial update
+    updateUI();
+
+    // Auto-update every 5 seconds (adjust as needed)
+    setInterval(updateUI, 1000);
+
 });
